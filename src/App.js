1924e24226a38515e62e874c74c83a399383a483
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-05-06 20:03:23 +0600

    secenes created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./seenes/home/Home.jsx";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [pathname]);
+  return null;
+};
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

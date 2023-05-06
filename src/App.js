6cb4e110b85aaaadc07ff0a2ce1e8e6dcfc87b59
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-05-06 22:44:30 +0600

    home added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./seenes/home/Home.jsx";
+import Navbar from "./seenes/global/Navbar.jsx";
+import ItemDetails from "./seenes/itemDetails/ItemDetails.jsx";
+import Confirmation from "./seenes/checkout/Confirmation.jsx";
+import CheckOut from "./seenes/checkout/CheckOut.jsx";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -13,10 +17,14 @@ const ScrollToTop = () => {
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/item/:id" element={<ItemDetails />} />
+          <Route path="/checkout" element={<CheckOut />} />
+          <Route path="/checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>

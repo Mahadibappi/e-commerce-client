00b18349b56786b1b7b5c2b1712f8ed898e355d5
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-05-08 17:32:12 +0600

    item added

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,14 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { shades } from "../theme";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+import { addToCart } from "../state";
 
 const Item = ({ item, width }) => {
-  const distpatch = useDispatch();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const [count, setCount] = useState(1);
   const [isHover, setIsHover] = useState(false);
@@ -24,8 +28,64 @@ const Item = ({ item, width }) => {
   } = image;
 
   return (
-    <Box>
-      <Typography>Hello</Typography>
+    <Box width={width}>
+      <Box
+        position="relative"
+        onMouseOver={() => setIsHover(true)}
+        onMouseOut={() => setIsHover(false)}
+      >
+        <img
+          width="300px"
+          height="400px"
+          src={`http://localhost:1337${url}`}
+          Alt={item.name}
+          onClick={() =>
+            navigate(`/item/${item.id}
+          `)
+          }
+          style={{ cursor: "pointer" }}
+        />
+        <Box
+          display={isHover ? "block" : "none"}
+          position="absolute"
+          left="0"
+          width="100%"
+          padding="0 5%"
+        >
+          <Box display="flex" justifyContent="space-between">
+            {/* amount */}
+            <Box
+              display="flex"
+              alignItems="center"
+              backgroundColor={shades.neutral[100]}
+              borderRadius="3px"
+            >
+              <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography color={shades.primary[300]}> {count}</Typography>
+              <IconButton onClick={() => setCount(count + 1)}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+            {/* button */}
+            <Button
+              onClick={() => dispatch(addToCart({ item: { ...item, count } }))}
+            >
+              Add to Cart
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+      <Box mt="30px">
+        <Typography variant="subtitle2" color={neutral.dark}>
+          {category
+            .replace(/([A-Z])/g, "$1")
+            .replace(/^./, (str) => str.toUpperCase())}
+        </Typography>
+        <Typography>{name}</Typography>
+        <Typography fontWeight="bold">${price}</Typography>
+      </Box>
     </Box>
   );
 };
